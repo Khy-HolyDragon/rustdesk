cdd50c3ba6847b4941a6d967d03e2f16d3a9a045
Khy-HolyDragon <zsnh_khy@outlook.com>
2024-06-28 14:16:49 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -97,7 +97,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["www.jiguangcg.com"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["longlovedan.familyds.com"];
 pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
